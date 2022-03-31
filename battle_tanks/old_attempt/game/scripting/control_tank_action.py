54c853504740c041a8a656b77d866072761c4ed4
Ryan Gunnar Schmidt <i1Schmidty@gmail.com>
2022-03-30 19:52:39 -0600

    move tanks progress

--- battle_tanks/old_attempt/game/scripting/control_tank_action.py
+++ battle_tanks/old_attempt/game/scripting/control_tank_action.py
@@ -8,10 +8,11 @@ class ControlTankAction(Action):
         self._keyboard_service = keyboard_service
         
     def execute(self, cast, script, callback):
-        tank = cast.get_first_actor(TANK_GROUP)
-        if self._keyboard_service.is_key_down(LEFT): 
-            tank.swing_left()
-        elif self._keyboard_service.is_key_down(RIGHT): 
-            tank.swing_right()  
-        else: 
-            tank.stop_moving()        
+        for i in range(2):
+            tank = cast.get_nth_actor(TANKS_GROUP, i)
+            if self._keyboard_service.is_key_down(P1_LEFT): 
+                tank.swing_left()
+            elif self._keyboard_service.is_key_down(P1_RIGHT): 
+                tank.swing_right()  
+            else: 
+                tank.stop_moving()        
